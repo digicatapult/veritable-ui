0fbe149303920b3911e256750902453a0fa867cf
Matthew Dean <matthew.dean@digicatapult.org.uk>
2024-05-09 09:22:37 +0100

    Fix issue with tsyringe resolving database

--- src/ioc.ts
+++ src/ioc.ts
@@ -2,10 +2,12 @@ import { IocContainer } from '@tsoa/runtime'
 import { container } from 'tsyringe'
 
 import { logger, Logger, type ILogger } from './logger'
+import Database from './models/db/index.js'
 
 container.register<ILogger>(Logger, {
   useValue: logger,
 })
+container.register(Database, { useValue: new Database() })
 
 export const iocContainer: IocContainer = {
   get: <T>(controller: { prototype: T }): T => {
