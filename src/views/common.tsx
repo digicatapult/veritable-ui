03947fb3f759edbc446c1c7b519ab3406ea916c2
Paulius <paulius.michelevicius@gmail.com>
2024-05-13 17:02:34 +0100

    View components: SideBar, Content, Heading and assets (#21)
    * VR-71: saving while waiting on assets
    
    * VR-71: adding all icons just exported from figma (will be renamed and sorted).
    
    * VR-71: renamed all assets to a more friendlier names.
    
    * VR-71: final rename and disabled class.
    
    * VR-71: assets again with sorted naming.
    
    * VR-71: disabled styling and content header.
    
    * VR-71: and with all asset elements alomng with some redirects e.g. hoime page.
    
    * VR-71: fixing failing build
    
    * VR-71: version bump.
    
    * VR-71: linting.
    
    * VR-71: test snapshot update.
    
    * VR-71: xss-scan has caught me - unsafe usage of JSX.
    git push
    
    * VR-71: a few styling impr: fonts/reset.css and titles to <a> tags.
    
    * VR-71: snapshot update.
    
    * VR-71: moving css.
    
    * VR-71: text formatting of reset.css.
    
    * VR-71: indentation mistake (css been a while).
    
    ---------
    
    Co-authored-by: Paulius Michelevicius <pmichelevicius@F4QVY4N44X.local>

--- src/views/common.tsx
+++ src/views/common.tsx
@@ -1,12 +1,75 @@
 import { Html } from '@kitajs/html'
 
+type PageProps = {
+  title: string
+  heading: string
+  url: string
+}
+
+/**
+ * Main menu/Nav
+ * @returns JSX - Sidarbar
+ */
 const SideBar = (): JSX.Element => (
-  <div class="flex-page side-bar">
-    <a>item 1</a>
+  <nav class="flex-page side-bar">
+    <img class="side-bar logo-container" src="/public/images/logo-square.svg" />
+    <a
+      title="categories"
+      href="#category"
+      class="side-bar icon disabled"
+      style={{ backgroundImage: 'url("/public/images/category.svg")' }}
+    />
+    <a
+      title="connections"
+      href="/connection"
+      class="side-bar icon active"
+      style={{ backgroundImage: 'url("/public/images/connection.svg")' }}
+    />
+    <a
+      title="storage"
+      href="#storage"
+      class="side-bar icon disabled"
+      style={{ backgroundImage: 'url("/public/images/folder.svg")' }}
+    />
+    <a
+      title="notifications"
+      href="#notification"
+      class="side-bar icon disabled"
+      style={{ backgroundImage: 'url("/public/images/notification.svg")' }}
+    />
+    <a
+      title="settings"
+      href="#settings"
+      class="side-bar icon disabled"
+      style={{ backgroundImage: 'url("/public/images/setting.svg")' }}
+    />
+  </nav>
+)
+
+/**
+ * Header for content wrapper class
+ * @param param0.heading = title of current content
+ * @returns JSX
+ */
+const ContentHeader = (props: { heading: string; url: string }): JSX.Element => (
+  <div class="content header">
+    <h1 class="header heading">{props.heading || 'unknown'}</h1>
+    <div class="header nav">
+      {/* is this meant to be /connections or just host/ */}
+      <a title="home" class="nav icon" href="/" />
+      <span class="url-separator">/</span>
+      <a title={props.heading || 'n/a'} href={props.url}>
+        {props.heading || 'unknown'}
+      </a>
+    </div>
   </div>
 )
 
-export const Page = (props: Html.PropsWithChildren<{ title: string }>): JSX.Element => (
+/**
+ * default page template: props.children = content
+ * @returns JSX - default page
+ */
+export const Page = (props: Html.PropsWithChildren<PageProps>): JSX.Element => (
   <>
     {'<!DOCTYPE html>'}
     <html lang="en">
@@ -20,8 +83,8 @@ export const Page = (props: Html.PropsWithChildren<{ title: string }>): JSX.Elem
       <body class="flex-page" hx-ext="json-enc">
         <SideBar />
         <div class="flex-page content">
-          <div class="content header">/header</div>
-          {props.children}
+          <ContentHeader heading={props.heading} url={props.url} />
+          <div class="content main">{props.children}</div>
         </div>
       </body>
     </html>
