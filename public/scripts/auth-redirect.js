5a2b6e90a0e03cc33f11c7cf53772e54e283bdf8
Matthew Dean <matthew.dean@digicatapult.org.uk>
2024-05-14 11:35:40 +0100

    Update snapshots

--- public/scripts/auth-redirect.js
+++ public/scripts/auth-redirect.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function () {
   document.body.addEventListener('htmx:beforeSwap', function (evt) {
     if (evt.detail.xhr.status === 401) {
-      // when a 401 occurs redirect to login
-      window.location.replace('/auth/login')
+      // when a 401 occurs redirect to login and then back to this page
+      window.location.replace('/auth/login?' + new URLSearchParams({ path: window.location.toString() }).toString())
     }
   })
 })
