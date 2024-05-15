de5634736fb3c29464f3a3ccac4841e897665f53
Paulius Michelevicius <pmichelevicius@F4QVY4N44X.local>
2024-05-15 10:55:46 +0100

    VR-63: removing the example.

--- src/controllers/__tests__/helpers.ts
+++ src/controllers/__tests__/helpers.ts
@@ -1,21 +1,5 @@
 import { Readable } from 'node:stream'
 
-import pino from 'pino'
-
-import Counter from '../../models/counter'
-import RootTemplates from '../../views/example'
-
-export const templateMock = {
-  Root: (s: string) => `root_${s}_root`,
-  Counter: () => `counter`,
-  Button: ({ disabled }) => `button_${disabled}_button`,
-} as RootTemplates
-export const mockLogger = pino({ level: 'silent' })
-export const counterMock = {
-  get: () => 42,
-  increment: () => 43,
-} as Counter
-
 export const toHTMLString = async (stream: Readable) => {
   const chunks: Uint8Array[] = []
   for await (const chunk of stream) {
