fd93dbef860ad9e1419eb523e834b6ece9465c09
Paulius Michelevicius <pmichelevicius@F4QVY4N44X.local>
2024-05-29 13:11:27 +0100

    VR-99: linting.

--- src/models/db/migrations/20240508094320_add_update_index.ts
+++ src/models/db/migrations/20240508094320_add_update_index.ts
@@ -1,6 +1,5 @@
 import { Knex } from 'knex'
 
-
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('connection', (def) => {
     def.index('updated_at', 'idx_connection_updated_at')
