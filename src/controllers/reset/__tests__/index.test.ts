8b26ffea87df6058f158f148a449b3c53ffb6729
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2024-08-13 12:49:42 +0100

    VR-185: validating reset.

--- src/controllers/reset/__tests__/index.test.ts
+++ src/controllers/reset/__tests__/index.test.ts
@@ -4,28 +4,37 @@ import { pino } from 'pino'
 import sinon from 'sinon'
 
 import { Env } from '../../../env.js'
-import { BadRequestError } from '../../../errors.js'
+import { BadRequestError, InternalError } from '../../../errors.js'
 import { ILogger } from '../../../logger.js'
 import Database from '../../../models/db/index.js'
 import VeritableCloudagent from '../../../models/veritableCloudagent.js'
 import { ResetController } from '../index.js'
 
+const fixtures = {
+  connections: [
+    { id: 'some-connection-id-1', agent_connection_id: 'some-agent-id-1' },
+    { id: 'some-connection-id-2', agent_connection_id: 'some-agent-id-2' },
+    { id: 'some-connection-id-3', agent_connection_id: 'some-agent-id-3' },
+  ],
+  connection_invites: [
+    { id: 'some-invite-id-1', connection_id: 'some-connection-id-1' },
+    { id: 'some-invite-id-2', connection_id: 'some-connection-id-2' },
+    { id: 'some-invite-id-3', connection_id: 'some-connection-id-3' },
+  ],
+}
+
 const dbMock = {
-  get: sinon.stub().callsFake(() =>
-    Promise.resolve([
-      { id: '1', agent_connection_id: '1' },
-      { id: '2', agent_connection_id: '2' },
-      { id: '3', agent_connection_id: '3' },
-    ])
-  ),
+  get: sinon.stub(),
   delete: sinon.stub(),
 }
 
 const cloudagentMock = {
   getConnectionById: sinon.stub().callsFake((id: string) => Promise.resolve([{ id }])),
-  getCredentialByConnectionId: sinon.stub().callsFake((id: string) => Promise.resolve([[{ id }]])),
-  deleteConnection: sinon.stub().resolves(),
-  deleteCredential: sinon.stub().resolves(),
+  getCredentialByConnectionId: sinon.stub().callsFake((id: string) => Promise.resolve([{ id }])),
+  deleteConnection: sinon.stub(),
+  deleteCredential: sinon.stub(),
+  getCredentials: sinon.stub().callsFake(() => Promise.resolve([])),
+  getConnections: sinon.stub().resolves([]),
 }
 
 const withMocks = (DEMO_MODE: Boolean = true) => {
@@ -50,15 +59,16 @@ const withMocks = (DEMO_MODE: Boolean = true) => {
   }
 }
 
-describe('ResetController', () => {
+describe.only('ResetController', () => {
   let result: unknown
+  before(() => {})
   afterEach(() => {
     sinon.restore()
   })
 
   describe('/reset ', () => {
     describe('if DEMO_MODE is set to false', () => {
-      beforeEach(async () => {
+      before(async () => {
         let { args } = withMocks(false)
         const controller = new ResetController(...args)
 
@@ -88,8 +98,15 @@ describe('ResetController', () => {
     })
 
     describe('if DEMO_MODE = true', () => {
-      beforeEach(async () => {
-        let { args } = withMocks(true)
+      before(async () => {
+        sinon.restore()
+        dbMock.get.onFirstCall().resolves(fixtures.connections)
+        dbMock.get.onSecondCall().resolves(fixtures.connection_invites)
+        dbMock.get.onSecondCall().resolves([])
+        dbMock.get.onThirdCall().resolves([])
+        dbMock.get.onCall(4).resolves([])
+        let { args } = withMocks()
+
         const controller = new ResetController(...args)
 
         try {
@@ -99,22 +116,66 @@ describe('ResetController', () => {
         }
       })
 
+      describe('and isReset() returns [false]', () => {
+        it('throws InternalError and returns error message', async () => {
+          let { args } = withMocks()
+
+          const controller = new ResetController(...args)
+          controller.isReset = sinon.stub().callsFake(() => false)
+
+          try {
+            result = (await controller.reset()) as unknown
+          } catch (err) {
+            result = err
+          }
+
+          expect(result)
+            .to.be.instanceOf(InternalError)
+            .that.has.property('message')
+            .that.is.equal('Error: reset failed')
+        })
+      })
+
       it('gets list of local connections and connections with credentials from cloudagent', () => {
         expect(dbMock.get.calledWith('connection')).to.be.equal(true)
-        expect(cloudagentMock.getCredentialByConnectionId.calledWith('1')).to.be.equal(true)
-        expect(cloudagentMock.getConnectionById.calledWith('1')).to.be.equal(true)
+        expect(dbMock.get.calledWith('connection_invite')).to.be.equal(true)
+        expect(cloudagentMock.getCredentialByConnectionId.calledWith('some-agent-id-1')).to.be.equal(true)
+        expect(cloudagentMock.getConnectionById.calledWith('some-agent-id-1')).to.be.equal(true)
+        expect(cloudagentMock.getCredentialByConnectionId.calledWith('some-agent-id-2')).to.be.equal(true)
+        expect(cloudagentMock.getConnectionById.calledWith('some-agent-id-2')).to.be.equal(true)
+        expect(cloudagentMock.getCredentialByConnectionId.calledWith('some-agent-id-3')).to.be.equal(true)
+        expect(cloudagentMock.getConnectionById.calledWith('some-agent-id-3')).to.be.equal(true)
         expect(cloudagentMock.getCredentialByConnectionId.callCount).to.be.equal(3)
         expect(cloudagentMock.getConnectionById.callCount).to.be.equal(3)
-        expect(cloudagentMock.deleteCredential.callCount).to.be.equal(3)
-        expect(cloudagentMock.deleteConnection.callCount).to.be.equal(3)
       })
 
       it('deletes connections and connection_invites locally', () => {
         expect(dbMock.delete.firstCall.args).to.deep.equal(['connection', {}])
         expect(dbMock.delete.secondCall.args).to.deep.equal(['connection_invite', {}])
+        expect(cloudagentMock.deleteCredential.firstCall.args).to.deep.equal(['some-agent-id-1'])
+        expect(cloudagentMock.deleteCredential.secondCall.args).to.deep.equal(['some-agent-id-2'])
+        expect(cloudagentMock.deleteCredential.thirdCall.args).to.deep.equal(['some-agent-id-3'])
+        expect(cloudagentMock.deleteCredential.callCount).to.be.equal(3)
+        expect(cloudagentMock.deleteConnection.callCount).to.be.equal(3)
       })
 
-      it('return 200', () => {
+      it('confirms that records have been removed', () => {
+        expect(cloudagentMock.getCredentials.callCount).to.be.equal(1)
+        expect(cloudagentMock.getConnections.callCount).to.be.equal(1)
+      })
+
+      it('return 200', async () => {
+        dbMock.get.callsFake(() => Promise.resolve([]))
+        let { args } = withMocks(true)
+
+        const controller = new ResetController(...args)
+        controller.isReset = sinon.stub().callsFake(() => true)
+
+        try {
+          result = (await controller.reset()) as unknown
+        } catch (err) {
+          result = err
+        }
         expect(result).to.deep.equal({ statusCode: 200 })
       })
     })
--- src/controllers/reset/index.ts
+++ src/controllers/reset/index.ts
@@ -2,7 +2,7 @@ import { Get, Hidden, Produces, Route, SuccessResponse } from 'tsoa'
 import { inject, injectable, singleton } from 'tsyringe'
 
 import { Env } from '../../env.js'
-import { BadRequestError } from '../../errors.js'
+import { BadRequestError, InternalError } from '../../errors.js'
 import { Logger, type ILogger } from '../../logger.js'
 import Database from '../../models/db/index.js'
 import type { ConnectionRow } from '../../models/db/types.js'
@@ -21,8 +21,29 @@ export class ResetController {
     private cloudagent: VeritableCloudagent,
     @inject(Logger) private logger: ILogger
   ) {
-    //    super()
     this.logger = logger.child({ controller: '/reset' })
+  }
+
+  // would be nice to restore to prior reset
+  public async isReset(): Promise<boolean> {
+    try {
+      const items: number[] = await Promise.all([
+        await this.db.get('connection', {}).then((res) => res.length),
+        await this.db.get('connection_invite', {}).then((res) => res.length),
+        await this.cloudagent.getCredentials().then((res) => res.length),
+        await this.cloudagent.getConnections().then((res) => res.length),
+      ])
+
+      return (
+        items.reduce((out: number, next: number) => {
+          return out + next
+        }, 0) !== 0
+      )
+    } catch (err: unknown) {
+      this.logger.debug('%s', err)
+
+      return false
+    }
   }
 
   /**
@@ -37,54 +58,49 @@ export class ResetController {
 
       throw new BadRequestError('DEMO_MODE is false')
     }
-    /*
-      - [x] get all connections
-      - [x] iterate over all connections
-      - [x] call cloudagent's API to get credentials for each connection_id
-      - [x] call /v1/connections/{connectionId} (DELETE) endpoint for each connection
-      - [x] call /v1/credentials/{credentialRecordId} (DELETE) endpoint for each credential
-      - [x] delete local connection and connection_invite records
-      - [ ] a check to confirm that the above has happened
-    */
-    const connections = (await this.db.get('connection')) || []
-    const agentConnectionIds: string[] = connections
-      .map((connection: ConnectionRow) => connection.agent_connection_id as string)
-      .filter(Boolean)
 
-    this.logger.debug('%j', { connections, agentConnectionIds })
+    try {
+      const connections = (await this.db.get('connection')) || []
+      const agentConnectionIds: string[] = connections
+        .map((connection: ConnectionRow) => connection.agent_connection_id as string)
+        .filter(Boolean)
 
-    const agent = {
-      credentials: await Promise.all(
-        agentConnectionIds.map((id: string) => {
-          return this.cloudagent.getCredentialByConnectionId(id)
-        })
-      ).then((credentials) => credentials.reduce((prev, next) => prev.concat(next), [])),
-      connections: await Promise.all(
-        agentConnectionIds.map((id: string) => {
-          return this.cloudagent.getConnectionById(id)
-        })
-      ),
+      this.logger.debug('found items: %j', { connections, agentConnectionIds })
+
+      const agent = {
+        credentials: await Promise.all(
+          agentConnectionIds.map((id: string) => {
+            return this.cloudagent.getCredentialByConnectionId(id)
+          })
+        ).then((credentials) => credentials.reduce((prev, next) => prev.concat(next), [])),
+        connections: await Promise.all(
+          agentConnectionIds.map((id: string) => {
+            return this.cloudagent.getConnectionById(id)
+          })
+        ),
+      }
+
+      await Promise.all([
+        ...agent.credentials.map(({ id }: { id: string }) => {
+          this.logger.debug('deleting credential from cloudagent %s: ', id)
+          return this.cloudagent.deleteCredential(id)
+        }),
+        ...agent.connections.map(({ id }: { id: string }) => {
+          this.logger.debug('deleting connection from cloudagent %s: ', id)
+          return this.cloudagent.deleteConnection(id)
+        }),
+        await this.db.delete('connection', {}),
+        await this.db.delete('connection_invite', {}),
+      ])
+
+      // confirm reset by calling isReset() method
+      if (!(await this.isReset())) {
+        throw new InternalError('reset failed')
+      }
+
+      return { statusCode: 200 }
+    } catch (err: unknown) {
+      throw new InternalError(err as string)
     }
-
-    await Promise.all(
-      agent.credentials.map(({ id }: { id: string }) => {
-        this.logger.debug('deleting credential from cloudagent %s: ', id)
-        return this.cloudagent.deleteCredential(id)
-      })
-    )
-
-    await Promise.all(
-      agent.connections.map(({ id }: { id: string }) => {
-        this.logger.debug('deleting connection from cloudagent %s: ', id)
-        return this.cloudagent.deleteConnection(id)
-      })
-    )
-
-    this.logger.debug('credentials found %j', { agent })
-
-    await this.db.delete('connection', {})
-    await this.db.delete('connection_invite', {})
-
-    return { statusCode: 200 }
   }
 }
