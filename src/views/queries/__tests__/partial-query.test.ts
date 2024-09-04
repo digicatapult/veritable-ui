e4be0c4f63c740b845feffbcf05d2e7cef8ad965
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2024-09-04 16:49:38 +0100

    VR-175: and another test scenario for partial = false.

--- src/views/queries/__tests__/partial-query.test.ts
+++ src/views/queries/__tests__/partial-query.test.ts
@@ -1,34 +1,59 @@
 import { expect } from 'chai'
 import { describe } from 'mocha'
+import { mockIds } from '../../../controllers/queries/__tests__/helpers.js'
 import { ConnectionRow } from '../../../models/db/types.js'
-import Scope3CarbonConsumptionResponseTemplates from '../responseCo2scope3.js'
+import Templates from '../responseCo2scope3.js'
 
+const templates = new Templates()
 const sampleDate = new Date(Date.UTC(2024, 6, 4))
-const connectionsExample: ConnectionRow[] = [
-  {
-    id: 'someId',
-    company_name: '<div>I own you</div>',
-    company_number: '3546783',
-    status: 'verified_both',
-    agent_connection_id: '233495875757',
-    pin_attempt_count: 0,
-    pin_tries_remaining_count: null,
-    created_at: sampleDate,
-    updated_at: sampleDate,
-  },
-]
+const connectionsExample: ConnectionRow[] = [{}, {}, {}, {}].map((_, i) => ({
+  id: `${mockIds.connectionId.substr(0, mockIds.connectionId.length - 1)}${i}`,
+  company_name: 'I own you',
+  company_number: '3546783',
+  status: 'verified_both',
+  agent_connection_id: '233495875757',
+  pin_attempt_count: 0,
+  pin_tries_remaining_count: null,
+  created_at: sampleDate,
+  updated_at: sampleDate,
+}))
 
 describe('Partial Query', () => {
+  describe('if partial is set to false', () => {
+    it('does not render connections list and leaves quantity input enabled', async () => {
+      const rendered = await templates.newScope3CarbonConsumptionResponseFormPage({
+        formStage: 'form',
+        company: {
+          company_name: 'VER123',
+          company_number: '3456789',
+          status: 'verified_both',
+          id: mockIds.companyId,
+          agent_connection_id: mockIds.agentConnectionId,
+          pin_tries_remaining_count: null,
+          pin_attempt_count: 0,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        queryId: mockIds.queryId,
+        quantity: 2,
+        partial: false,
+        connections: connectionsExample,
+        productId: 'product-id-test',
+      })
+
+      expect(rendered).to.matchSnapshot()
+    })
+  })
+
   it('renders partial query table with connections that are verified', async () => {
-    const templates = new Scope3CarbonConsumptionResponseTemplates()
     const rendered = await templates.newScope3CarbonConsumptionResponseFormPage({
       formStage: 'form',
       company: {
         company_name: 'VER123',
         company_number: '3456789',
         status: 'verified_both',
-        id: 'aa000000-0000-0000-0000-aabbccddee00',
-        agent_connection_id: 'agentId',
+        id: mockIds.companyId,
+        agent_connection_id: mockIds.agentConnectionId,
         pin_tries_remaining_count: null,
         pin_attempt_count: 0,
         created_at: new Date(),
