2076bbbfbb1f2ccb1ef1382c8d2461b73b5ff0f5
Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>
2024-09-18 10:46:12 +0100

    regexed string and pin out

--- test/e2e/connection-from-alice-to-bob.spec.ts
+++ test/e2e/connection-from-alice-to-bob.spec.ts
@@ -1,82 +1,120 @@
 import { expect, test } from '@playwright/test'
 
-test('Connection from Alice to Bob ', async ({ browser }) => {
-  const context = await browser.newContext()
-  const page = await context.newPage()
-  await page.goto('http://localhost:3000')
+test.describe('Connection from Alice to Bob', () => {
+  test('Connection from Alice to Bob ', async ({ browser }) => {
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('http://localhost:3000')
 
-  const url = page.url()
+    const url = page.url()
 
-  expect(url).toContain(
-    'http://localhost:3080/realms/veritable/protocol/openid-connect/auth?response_type=code&client_id=veritable-ui&redirect_uri=http'
-  )
-  // Wait for the "Register" link to be visible on the Keycloak login page and click
-  await page.waitForSelector('a[href*="/realms/veritable/login-actions/registration"]')
-  await page.click('a[href*="/realms/veritable/login-actions/registration"]')
+    expect(url).toContain(
+      'http://localhost:3080/realms/veritable/protocol/openid-connect/auth?response_type=code&client_id=veritable-ui&redirect_uri=http'
+    )
+    // Wait for the "Register" link to be visible on the Keycloak login page and click
+    await page.waitForSelector('a[href*="/realms/veritable/login-actions/registration"]')
+    await page.click('a[href*="/realms/veritable/login-actions/registration"]')
 
-  // Wait for the registration page to load
-  await page.waitForURL('**/realms/veritable/login-actions/registration**')
-  expect(page.url()).toContain('/realms/veritable/login-actions/registration')
+    // Wait for the registration page to load
+    await page.waitForURL('**/realms/veritable/login-actions/registration**')
+    expect(page.url()).toContain('/realms/veritable/login-actions/registration')
 
-  // Fill in the fields
-  await page.fill('#username', 'name')
-  await page.fill('#password', 'password')
-  await page.fill('#password-confirm', 'password')
-  await page.fill('#email', 'email@testmail.com')
-  await page.fill('#firstName', 'name')
-  await page.fill('#lastName', 'lastname')
-  await page.click('input[type="submit"][value="Register"]')
-  await page.waitForURL('http://localhost:3000')
+    // Fill in the fields
+    await page.fill('#username', 'name')
+    await page.fill('#password', 'password')
+    await page.fill('#password-confirm', 'password')
+    await page.fill('#email', 'email@testmail.com')
+    await page.fill('#firstName', 'name')
+    await page.fill('#lastName', 'lastname')
+    await page.click('input[type="submit"][value="Register"]')
+    await page.waitForURL('http://localhost:3000')
 
-  // Wait for the link to be visible (make sure the page is fully loaded) and click
-  await page.waitForSelector('a[href="/connection"]')
-  await page.click('a[href="/connection"]')
+    // Wait for the link to be visible (make sure the page is fully loaded) and click
+    await page.waitForSelector('a[href="/connection"]')
+    await page.click('a[href="/connection"]')
 
-  // Wait for the page to navigate to the expected URL after clicking
-  await page.waitForURL('**/connection')
-  expect(page.url()).toContain('/connection')
+    // Wait for the page to navigate to the expected URL after clicking
+    await page.waitForURL('**/connection')
+    expect(page.url()).toContain('/connection')
 
-  // Wait for the "Invite New Connection" button to be visible and click
-  await page.waitForSelector('text=Invite New Connection')
-  await page.click('text=Invite New Connection')
+    // Wait for the "Invite New Connection" button to be visible and click
+    await page.waitForSelector('text=Invite New Connection')
+    await page.click('text=Invite New Connection')
 
-  // Wait for the page to navigate to the expected URL after clicking
-  await page.waitForURL('**/connection/new')
-  expect(page.url()).toContain('/connection/new')
+    // Wait for the page to navigate to the expected URL after clicking
+    await page.waitForURL('**/connection/new')
+    expect(page.url()).toContain('/connection/new')
 
-  // Fill in the fields
-  await page.fill('#new-invite-company-number-input', '07964699')
-  await page.fill('#new-invite-email-input', 'alice@testmail.com')
-  // Wait for the textarea to be rendered
-  await page.waitForSelector('#new-connection-invite-input textarea[name="invite"]')
+    // Fill in the fields
+    await page.fill('#new-invite-company-number-input', '07964699')
+    await page.fill('#new-invite-email-input', 'alice@testmail.com')
 
-  // Assert that the textarea is present and contains the placeholder text
-  const textarea = await page.$('#new-connection-invite-input textarea[name="invite"]')
-  // Ensure the textarea is not null before proceeding
-  if (!textarea) {
-    throw new Error('Textarea was not found on the page')
-  }
+    await page.waitForTimeout(10000)
 
-  // Need to mock the successful look up to the company house
+    await page.evaluate(() => document.querySelector('#new-connection-feedback'), { timeout: 10000 })
+    await page.evaluate(() => document.querySelector('.positive-feedback'), { timeout: 10000 })
 
-  // Wait for the textarea content to update
-  //   await page.waitForFunction((el) => (el as HTMLTextAreaElement).value.includes('Registered Office Address'), textarea)
-  //   // Retrieve the current value of the textarea
-  //   const textareaContent = await textarea.evaluate((el) => (el as HTMLTextAreaElement).value)
+    // Ensure the feedback element is present and get its text content
+    const feedbackElement = await page.$('#new-connection-feedback')
+    if (!feedbackElement) {
+      throw new Error('Feedback element was not found on the page')
+    }
 
-  // Define the expected content
-  //   const expectedContent = `Registered Office Address
-  //   DIGITAL CATAPULT, Level 9, 101 Euston Road, London, NW1 2RA
+    // Get the text content of the feedback element
+    const textContent = await feedbackElement.textContent()
+    if (!textContent) {
+      throw new Error('Feedback element does not contain any text')
+    }
 
-  //   Company Status
-  //   active`
+    // Assert that the feedback element contains the expected text
 
-  // Assert that the textarea content matches the expected text
-  //   expect(textareaContent).toBe(expectedContent)
+    expect(textContent).toContain('DIGITAL CATAPULT')
+    expect(textContent).toContain('101 Euston Road')
 
-  // Now click on the "Continue" button
-  await page.click('button[type="submit"][name="action"][value="continue"]')
+    // Now click on the "Continue" button
+    await page.click('button[type="submit"][name="action"][value="continue"]')
+    await page.waitForTimeout(3000)
 
-  // Wait for any subsequent page or action to occur, if needed
-  await page.waitForNavigation() // Adjust the navigation wait as per your application's behavior
+    await page.evaluate(() => document.querySelector('#new-connection-confirmation-text'), { timeout: 10000 })
+    const confirmationElement = await page.$('#new-connection-confirmation-text')
+    if (!confirmationElement) {
+      throw new Error('Confirmation element was not found on the page')
+    }
+    const confirmationElementText = await confirmationElement.textContent()
+
+    if (!confirmationElementText) {
+      throw new Error('Confirmation element does not contain any text')
+    }
+
+    expect(confirmationElementText).toContain('Please confirm the details of the connection before sending')
+    expect(confirmationElementText).toContain('Company House Number: 07964699')
+    expect(confirmationElementText).toContain('Email Address: alice@testmail.com')
+
+    // Now click on the "Submit" button
+    await page.click('button[type="submit"][name="action"][value="submit"]')
+    await page.waitForTimeout(3000)
+    await page.evaluate(() => document.querySelector('#new-connection-confirmation-text'), { timeout: 10000 })
+    const confirmationElement2 = await page.$('#new-connection-confirmation-text')
+    if (!confirmationElement2) {
+      throw new Error('Confirmation element was not found on the page')
+    }
+    const confirmationElementText2 = await confirmationElement2.textContent()
+
+    if (!confirmationElementText2) {
+      throw new Error('Confirmation element does not contain any text')
+    }
+
+    expect(confirmationElementText2).toContain(
+      'Your connection invitation has been sent. Please wait for their verification. As the post may take 2-3 days to arrive, please wait for their verification and keep updated by viewing the verification status.'
+    )
+
+    // Back to connections page ... should this be back home?
+    await page.waitForSelector('a[href="/connection"]')
+    await page.click('a[href="/connection"]')
+    await page.waitForTimeout(3000)
+    await page.waitForURL('**/connection')
+    expect(page.url()).toContain('/connection')
+
+    // Create a new tab (page) and go to SMTP server
+  })
 })
---
+++ test/e2e/helpers/smtpEmails.ts
@@ -0,0 +1,157 @@
+import { expect } from '@playwright/test'
+import http from 'http'
+import { z } from 'zod'
+
+const ToSchema = z.array(z.string())
+
+const EmailItemSchema = z.object({
+  isRelayed: z.boolean(),
+  deliveredTo: z.string().email(),
+  id: z.string().uuid(),
+  from: z.string().email(),
+  to: ToSchema,
+  receivedDate: z.string().datetime(),
+  subject: z.string(),
+  attachmentCount: z.number().int(),
+  isUnread: z.boolean(),
+})
+
+const EmailResponseSchema = z.object({
+  results: z.array(EmailItemSchema),
+})
+
+type Emails = {
+  isRelayed: boolean
+  deliveredTo: string
+  id: string
+  from: string
+  to: string[]
+  receivedDate: string
+  subject: string
+  attachmentCount: number
+  isUnread: boolean
+}
+
+async function checkEmails(): Promise<{ inviteEmail: Emails; adminEmail: Emails }> {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'localhost',
+      port: 5000,
+      path: '/api/messages',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    const req = http.request(options, (res) => {
+      let data = ''
+      res.on('data', (chunk) => (data += chunk))
+      res.on('end', () => {
+        try {
+          const messages = JSON.parse(data)
+          const parsedMessages = EmailResponseSchema.parse(messages)
+          const results = parsedMessages.results
+
+          expect(Array.isArray(results)).toBeTruthy()
+          expect(results).toHaveLength(2)
+          // Get the adminEmail and inviteEmail from validateEmails
+          const { inviteEmail, adminEmail } = validateEmails(results)
+
+          resolve({ inviteEmail, adminEmail })
+        } catch (error) {
+          reject(error)
+        }
+      })
+    })
+
+    req.on('error', (error) => reject(error))
+    req.end()
+  })
+}
+
+// Function that checks specific email content and returns both emails
+function validateEmails(results: Emails[]): {
+  inviteEmail: Emails
+  adminEmail: Emails
+} {
+  // Invite email assertions
+  const inviteEmail = results.find((msg) => msg.subject === 'Veritable invite')
+  if (inviteEmail) {
+    expect(inviteEmail.to).toHaveLength(1)
+    expect(inviteEmail.to[0]).toContain('alice@testmail.com')
+  } else {
+    throw new Error('No email found with the subject "Veritable invite".')
+  }
+
+  // Admin email assertions
+  const adminEmail = results.find((msg) => msg.subject === 'Action required: process veritable invitation')
+  if (adminEmail) {
+    expect(adminEmail.to).toHaveLength(1)
+    expect(adminEmail.to[0]).toContain('admin@veritable.com')
+  } else {
+    throw new Error('No email found with the subject "Action required: process veritable invitation".')
+  }
+
+  // Return both email objects
+  return { inviteEmail, adminEmail }
+}
+
+async function extractPin(emailId: string): Promise<string | null> {
+  const apiUrl = `http://localhost:5000/api/messages/${emailId}/raw`
+
+  try {
+    // Fetch the raw email content
+    const response = await fetch(apiUrl)
+    if (!response.ok) {
+      throw new Error(`Error fetching email: ${response.statusText}`)
+    }
+
+    const rawEmailContent = await response.text()
+
+    // Regex pattern to find the PIN
+    const pinPattern = /PIN:\s*(\d+)/
+    const match = rawEmailContent.match(pinPattern)
+
+    // Return the PIN if found
+    if (match && match[1]) {
+      return match[1]
+    } else {
+      return null // Return null if PIN is not found
+    }
+  } catch (error) {
+    console.error('Failed to fetch email content:', error)
+    throw error // Rethrow the error to propagate it
+  }
+}
+
+async function extractInvite(emailId: string): Promise<string | null> {
+  const apiUrl = `http://localhost:5000/api/messages/${emailId}/raw`
+
+  try {
+    const response = await fetch(apiUrl)
+    if (!response.ok) {
+      throw new Error(`Error fetching email: ${response.statusText}`)
+    }
+
+    const rawEmailContent = await response.text()
+    // Regex pattern to find the base64 string
+    const base64Pattern = /<p>(eyJ[^<]+)<\/p>/
+    const match = rawEmailContent.match(base64Pattern)
+
+    if (match && match[1]) {
+      const cleanedString = match[1]
+        .replace(/\s+/g, '') // Remove all whitespace including line breaks
+        .replace(/=/g, '') // Remove equal signs
+
+      return cleanedString
+    } else {
+      return null
+    }
+  } catch (error) {
+    console.error('Failed to fetch email content:', error)
+    throw error
+  }
+}
+
+export { checkEmails, extractInvite, extractPin }
