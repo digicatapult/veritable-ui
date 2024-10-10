d43f905d34cfe7b9acbe143f11d41ded0b785c6f
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2024-10-10 12:48:16 +0100

    VR-245: making parent_id a foreign key.

--- src/models/db/migrations/20241010125209_query_tables_additional_column-2.ts
+++ src/models/db/migrations/20241010125209_query_tables_additional_column-2.ts
@@ -2,7 +2,13 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('query', (def) => {
-    def.string('parent_id').defaultTo(null)
+    def.uuid('parent_id').nullable().defaultTo(null)
+    def
+      .foreign('parent_id', 'fk_partial_query_parent_id')
+      .references('id')
+      .inTable('query')
+      .onDelete('cascade')
+      .onUpdate('cascade')
   })
 }
 
