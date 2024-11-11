ad1ad1f535310e7a29de97d9c9beb2da00e352d9
Paulius <paulius.michelevicius@gmail.com>
2024-11-11 14:35:43 +0000

    Updating test harness and adding a new test suite for submitting a query (#229)
    * VR-294: withVerified connection helper
    
    * VR-294: linting and version bump.
    
    * VR-294: updating other tests to use new helper (withConnection).
    
    * VR-294: adding test scenarios for putting a query through.
    
    * VR-294: a few renames.
    
    * VR-294: linting.
    
    * VR-294: merge and linting.
    
    * VR-294: version bump.
    
    * VR-294: unwanted change leftover from merge.
    
    * VR-294: removing a sketch file also should have not been staged.
    
    * VR-294: clean up and checkEmails refactor to search by to/from/subject
    
    * VR-294: a few copy changes, renames - sanity checks.
    
    * VR-294: start of query response test suite.
    
    * VR-294: start on query response test suite.
    
    * VR-294: version bump.
    
    * VR-294: removing test suite from another story (accidental stage).
    
    * VR-294: playwright config.:
    
    * VR-294: use headless (as per error in github action).
    
    * VR-294: linting.
    
    * VR-294: removing --debug from playwright (leftover).
    
    * VR-294: failing in ci/cd, re-adding waitForURL().
    
    * VR-294: getting before/after inline across.
    
    * VR-294: removing duplicate beforeEach method from reset.
    
    * VR-294: config leftovers.
    
    * VR-294: created a generic method for cleaning up eg, /reset.
    
    * VR-294: version bump.
    
    * VR-294: typo.
    
    * VR-294: giving some extra time for events to fire and update db.
    
    * VR-294: flakiness in reset test suite, updated to not use waitForSelector.
    
    * VR-294: restoring test.timeout to reset-app-spec.
    
    * VR-294: correct delay time.
    
    * VR-294: page.waitForTimeout(3000) work better than delay in the helper.
    
    * VR-294: version bump.
    
    * VR-294: version bump.
    
    * VR-294: more time for connection replication.
    
    * VR-294: increase timeout to 5s.
    
    * VR-294: restoring reset-app.spec.ts
    
    * VR-294: removing double beforeEach in reset-app.
    
    * VR-294: version bump.
    
    * VR-294: removing assertations from test harness.
    
    * VR-294: unused declarations.
    
    * VR-294: add delay of 2s before click on submit query.
    
    * VR-294: await for connection status in harness.g

--- test/e2e/helpers/setupConnection.ts
+++ test/e2e/helpers/setupConnection.ts
@@ -1,32 +1,82 @@
 import 'reflect-metadata'
-import { fetchPost } from '../../helpers/routeHelper.js'
-import { checkEmails, extractInvite, extractPin, getHostPort } from './smtpEmails.js'
 
-export async function withConnection(smtp4devUrl: string, aliceUrl: string, bobUrl: string) {
-  // Send an invite from Alice to Bob
-  await fetchPost(`${aliceUrl}/connection/new/create-invitation`, {
+import { fetchGet, fetchPost } from '../../helpers/routeHelper.js'
+import { delay } from '../../helpers/util.js'
+import { checkEmails, extractInvite, extractPin } from './smtpEmails.js'
+
+export async function withConnection(invitatorUrl: string, receiverUrl: string) {
+  const uuidRegex = /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}/g
+
+  await fetchPost(`${invitatorUrl}/connection/new/create-invitation`, {
     companyNumber: '04659351',
     email: 'alice@testmail.com',
     action: 'submit',
   })
 
-  // Get pin and invite
-  const { host, port } = getHostPort(smtp4devUrl)
-  if (host === null || port === null) {
-    throw new Error(`Unspecified smtp4dev host or port ${smtp4devUrl}`)
-  }
-  const { inviteEmail, adminEmail } = await checkEmails(host, port)
-  const extractedPin = await extractPin(adminEmail.id, smtp4devUrl)
-  if (!extractedPin) throw new Error('PIN for Bob was not found.')
-  if (extractedPin.length !== 6) {
-    throw new Error('Pin does not have the expected length.')
-  }
-  const invite = await extractInvite(inviteEmail.id, smtp4devUrl)
-  if (!invite) throw new Error('Invitation for Bob was not found.')
+  // Get pin and invite for the new holder
+  const adminEmail = await checkEmails('admin@veritable.com')
+  const invitatorPin = await extractPin(adminEmail.id)
+  if (!invitatorPin) throw new Error(`PIN for ${receiverUrl} was not found.`)
 
-  // Use invite and from Bob's side
-  await fetchPost(`${bobUrl}/connection/new/receive-invitation`, {
-    invite: invite,
+  const inviteEmail = await checkEmails('alice@testmail.com')
+  const inviteBase64 = await extractInvite(inviteEmail.id)
+  if (!inviteBase64) throw new Error('Invitation for Bob was not found.')
+
+  // verify and validate a new connection - holder
+  const receive = await fetchPost(`${receiverUrl}/connection/new/receive-invitation`, {
+    invite: inviteBase64,
     action: 'createConnection',
   })
+  const [holderConnectionId] = (await receive.text()).match(uuidRegex) || []
+  if (!holderConnectionId) throw new Error(`Connection was not found`)
+
+  let retries = 30
+
+  while (retries) {
+    await delay(500)
+    const receiverConnection = await fetchGet(`${receiverUrl}/connection?search=DIGI`)
+    const [receiverConnectionStatus] = (await receiverConnection.text()).match('Verification Code') || []
+    if (receiverConnectionStatus) {
+      retries = 0
+    } else if (retries === 1 && !receiverConnectionStatus) {
+      throw new Error('timeout')
+    } else {
+      retries--
+    }
+  }
+
+  await fetchPost(`${receiverUrl}/connection/${holderConnectionId}/pin-submission`, {
+    action: 'submitPinCode',
+    pin: invitatorPin,
+    stepCount: '3',
+  })
+
+  const receiverEmail = await checkEmails('admin@veritable.com')
+  const receiverPin = await extractPin(receiverEmail.id)
+
+  const connections = await fetchGet(`${invitatorUrl}/connection?search=OFFSHORE`)
+  const [invitatorConnectionId] = (await connections.text()).match(uuidRegex) || []
+  if (!receiverPin || !invitatorConnectionId) {
+    throw new Error(`PIN ${receiverPin} or Connection ID ${invitatorConnectionId} not found`)
+  }
+
+  await fetchPost(`${invitatorUrl}/connection/${invitatorConnectionId}/pin-submission`, {
+    action: 'submitPinCode',
+    pin: receiverPin,
+    stepCount: '2',
+  })
+
+  retries = 20
+  while (retries) {
+    await delay(500)
+    const invitatorConnection = await fetchGet(`${invitatorUrl}/connection?search=OFFSHORE`)
+    const [invitatorConnectionStatus] = (await invitatorConnection.text()).match('Send Query') || []
+    if (invitatorConnectionStatus) {
+      retries = 0
+    } else if (retries === 1 && !invitatorConnectionStatus) {
+      throw new Error('timeout')
+    } else {
+      retries--
+    }
+  }
 }
--- test/e2e/reset-app.spec.ts
+++ test/e2e/reset-app.spec.ts
@@ -1,12 +1,6 @@
 import { expect, Page, test } from '@playwright/test'
 import 'reflect-metadata'
-import {
-  CustomBrowserContext,
-  withCleanAlice,
-  withCleanAliceBobEmail,
-  withLoggedInUser,
-  withRegisteredAccount,
-} from './helpers/registerLogIn.js'
+import { cleanup, CustomBrowserContext, withLoggedInUser, withRegisteredAccount } from './helpers/registerLogIn.js'
 import { withConnection } from './helpers/setupConnection.js'
 
 test.describe('Resetting app', () => {
@@ -15,31 +9,23 @@ test.describe('Resetting app', () => {
 
   const baseUrlAlice = process.env.VERITABLE_ALICE_PUBLIC_URL || 'http://localhost:3000'
   const baseUrlBob = process.env.VERITABLE_BOB_PUBLIC_URL || 'http://localhost:3001'
-  const smtp4devUrl = process.env.VERITABLE_SMTP_ADDRESS || 'http://localhost:5001'
 
   test.beforeAll(async ({ browser }) => {
     context = await browser.newContext()
     page = await context.newPage()
     await withRegisteredAccount(page, context, baseUrlAlice)
   })
-  test.beforeEach(async () => {
-    await withCleanAlice(baseUrlAlice)
-    page = await context.newPage()
-    await withLoggedInUser(page, context, baseUrlAlice)
-  })
 
   test.afterEach(async () => {
-    await withCleanAlice(baseUrlAlice)
-    await withCleanAliceBobEmail(baseUrlAlice, baseUrlBob, smtp4devUrl)
     await page.close()
+    await cleanup([baseUrlAlice, baseUrlBob])
   })
   test.beforeEach(async () => {
-    await withCleanAliceBobEmail(baseUrlAlice, baseUrlBob, smtp4devUrl)
-    await withCleanAlice(baseUrlAlice)
+    await cleanup([baseUrlAlice, baseUrlBob])
     page = await context.newPage()
     await withLoggedInUser(page, context, baseUrlAlice)
 
-    await withConnection(smtp4devUrl, baseUrlAlice, baseUrlBob)
+    await withConnection(baseUrlAlice, baseUrlBob)
   })
 
   test('Reset all on Alice', async () => {
@@ -49,9 +35,9 @@ test.describe('Resetting app', () => {
       await page.waitForSelector('a[href="/connection"]')
       await page.click('a[href="/connection"]')
       await page.waitForURL('**/connection')
-      await page.waitForSelector('div.list-item-status[data-status="warning"]')
-      const statusText = await page.textContent('div.list-item-status[data-status="warning"]')
-      expect(statusText).toContain('Verification Code Required')
+      await page.waitForSelector('div.list-item-status[data-status="success"]')
+      const statusText = await page.textContent('div.list-item-status[data-status="success"]')
+      expect(statusText).toContain('Connected')
     })
 
     await test.step('Click reset on Alice', async () => {
@@ -61,6 +47,7 @@ test.describe('Resetting app', () => {
       await page.waitForSelector('#reset-btn')
       await page.click('#reset-btn')
     })
+
     await test.step('Check there are no connections on Alice', async () => {
       await page.goto(`${baseUrlAlice}/connection`)
       await page.waitForURL('**/connection')
