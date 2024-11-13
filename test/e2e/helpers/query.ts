cb17dda13f855a3e7ce1ff6db5e0df96e3776e9c
Paulius <paulius.michelevicius@gmail.com>
2024-11-13 12:29:41 +0000

    VR-295: helper and query response e2e test suite. (#236)
    * VR-295: helper and query response e2e test suite.
    
    * VR-295: assert home page as well (after success).
    
    * VR-295: version bump.
    
    * VR-295: unwanted file.
    
    * VR-295: typo in assert (should have used env var.

---
+++ test/e2e/helpers/query.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata'
+
+import { fetchGet, fetchPost } from '../../helpers/routeHelper.js'
+
+export const co2QueryContent = {
+  productId: 'E2E-DC001',
+  quantity: '100',
+}
+
+export async function withQueryRequest(requesterUrl: string) {
+  const uuidRegex = /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}/
+  const connections = await fetchGet(`${requesterUrl}/connection`)
+  const html = await connections.text()
+  const [connectionId] = html.match(uuidRegex) || []
+  if (!connectionId) {
+    throw new Error(`Connection not found ${connectionId}`)
+  }
+
+  await fetchPost(`${requesterUrl}/queries/new/scope-3-carbon-consumption`, {
+    connectionId,
+    ...co2QueryContent,
+  })
+}
