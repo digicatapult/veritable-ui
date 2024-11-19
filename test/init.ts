3fb559cb7ed2e01b6440a0894d64962755765b47
Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>
2024-11-19 09:53:23 +0000

    cleanup containers

--- test/init.ts
+++ test/init.ts
@@ -2,6 +2,7 @@ import { use } from 'chai'
 import 'reflect-metadata'
 
 import chaiJestSnapshot from 'chai-jest-snapshot'
+import { StartedTestContainer } from 'testcontainers'
 import { migrateDatabase } from './helpers/db.js'
 import {
   bringUpAliceDependenciesContainers,
@@ -10,15 +11,43 @@ import {
   bringUpSharedContainers,
 } from './testcontainers/testcontainersSetup.js'
 
+let sharedContainers: StartedTestContainer[]
+let aliceDepsContainers: StartedTestContainer[]
+let bobContainers: StartedTestContainer[]
+let charlieContainers: StartedTestContainer[]
+
 before(async function () {
-  await bringUpSharedContainers()
-  await bringUpAliceDependenciesContainers()
-  await bringUpBobContainers()
-  await bringUpCharlieContainers()
+  sharedContainers = await bringUpSharedContainers()
+  aliceDepsContainers = await bringUpAliceDependenciesContainers()
+  bobContainers = await bringUpBobContainers()
+  charlieContainers = await bringUpCharlieContainers()
 
   await migrateDatabase()
   use(chaiJestSnapshot)
   chaiJestSnapshot.resetSnapshotRegistry()
+})
+
+after(async function () {
+  await Promise.all(
+    aliceDepsContainers.map(async function (container) {
+      await container.stop()
+    })
+  )
+  await Promise.all(
+    bobContainers.map(async function (container) {
+      await container.stop()
+    })
+  )
+  await Promise.all(
+    charlieContainers.map(async function (container) {
+      await container.stop()
+    })
+  )
+  await Promise.all(
+    sharedContainers.map(async function (container) {
+      await container.stop()
+    })
+  )
 })
 
 beforeEach(function () {
