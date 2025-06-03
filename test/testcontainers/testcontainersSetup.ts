5eb1760ee189a119aae08404c078a67f724b52e4
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-06-03 20:34:57 +0100

    Bump version in testcontainersSetup.ts

--- test/testcontainers/testcontainersSetup.ts
+++ test/testcontainers/testcontainersSetup.ts
@@ -234,7 +234,7 @@ export async function composeKeycloakContainer(
   network: StartedNetwork,
   keycloakDataPath: string
 ): Promise<StartedTestContainer> {
-  const keycloakContainer = await new GenericContainer('quay.io/keycloak/keycloak:26.0.5')
+  const keycloakContainer = await new GenericContainer('quay.io/keycloak/keycloak:26.2.5')
     .withName('keycloak')
     .withExposedPorts({
       container: 8080,
