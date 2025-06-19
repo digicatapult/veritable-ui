427c8d9a9b0ea41a6e1f4fd047452da3d03cb6c7
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-06-19 10:07:09 +0100

    hardcode some envars in ui

--- test/testcontainers/testcontainersSetup.ts
+++ test/testcontainers/testcontainersSetup.ts
@@ -37,29 +37,11 @@ interface VeritableUIConfig extends PostgresValuesInterface {
   hostPort: number
   containerPort: number
   postgresPort: string
-  idpPublicUrlPrefix: string
   cloudagentAdminOrigin: string
   cloudagentAdminWsOrigin: string
-  invitationPinSecret?: string
   invitationFromCompanyNumber: string
   publicUrl: string
   companyProfileApiKey: string
-  companyHouseApiUrl?: string
-  idpInternalUrlPrefix?: string
-  smtpHost?: string
-  smtpPass?: string
-  smtpPort?: string
-  smtpUser?: string
-  demoMode?: string
-  smtpSecure?: string
-  emailTransport?: string
-  cookieSessionKeys?: string
-  idpClientId?: string
-  nodeEnv?: 'production' | 'development' | 'test'
-  logLevel?: 'debug' | 'info' | 'warn' | 'error'
-  issuanceDidPolicy?: string
-  issuanceSchemaPolicy?: string
-  issuanceCredDefPolicy?: string
 }
 
 const network = await new Network().start()
@@ -91,7 +73,6 @@ export async function bringUpAliceUIContainer() {
     hostPort: 3000,
     containerPort: 3000,
     postgresPort: '5432',
-    idpPublicUrlPrefix: 'http://localhost:3080/realms/veritable/protocol/openid-connect',
     cloudagentAdminOrigin: 'http://veritable-cloudagent-alice:3000',
     cloudagentAdminWsOrigin: 'ws://veritable-cloudagent-alice:3000',
     invitationFromCompanyNumber: '07964699',
@@ -110,7 +91,6 @@ export async function bringUpBobUIContainer() {
     hostPort: 3001,
     containerPort: 3000,
     postgresPort: '5432',
-    idpPublicUrlPrefix: 'http://localhost:3080/realms/veritable/protocol/openid-connect',
     cloudagentAdminOrigin: 'http://veritable-cloudagent-bob:3000',
     cloudagentAdminWsOrigin: 'ws://veritable-cloudagent-bob:3000',
     invitationFromCompanyNumber: '04659351',
@@ -129,7 +109,6 @@ export async function bringUpCharlieUIContainer() {
     hostPort: 3002,
     containerPort: 3000,
     postgresPort: '5432',
-    idpPublicUrlPrefix: 'http://localhost:3080/realms/veritable/protocol/openid-connect',
     cloudagentAdminOrigin: 'http://veritable-cloudagent-charlie:3000',
     cloudagentAdminWsOrigin: 'ws://veritable-cloudagent-charlie:3000',
     invitationFromCompanyNumber: '10016023',
@@ -386,31 +365,10 @@ export async function veritableUIContainer(network: StartedNetwork, env: Veritab
     hostPort,
     postgresPort,
     publicUrl,
-    idpPublicUrlPrefix,
     invitationFromCompanyNumber,
     cloudagentAdminOrigin,
     cloudagentAdminWsOrigin,
     companyProfileApiKey,
-    postgresDb,
-    nodeEnv = 'production',
-    logLevel = 'trace',
-    postgresUser = 'postgres',
-    postgresPassword = 'postgres',
-    cookieSessionKeys = 'secret',
-    idpClientId = 'veritable-ui',
-    idpInternalUrlPrefix = 'http://keycloak:8080/realms/veritable/protocol/openid-connect',
-    invitationPinSecret = 'secret',
-    issuanceDidPolicy = 'EXISTING_OR_NEW',
-    issuanceSchemaPolicy = 'EXISTING_OR_NEW',
-    issuanceCredDefPolicy = 'EXISTING_OR_NEW',
-    smtpHost = 'smtp4dev',
-    smtpPass = '',
-    smtpPort = '25',
-    smtpUser = '',
-    emailTransport = 'SMTP_EMAIL',
-    companyHouseApiUrl = 'https://api.company-information.service.gov.uk',
-    demoMode = 'true',
-    smtpSecure = 'false',
   } = env
 
   const base = await GenericContainer.fromDockerfile('./').build()
@@ -422,33 +380,33 @@ export async function veritableUIContainer(network: StartedNetwork, env: Veritab
       host: hostPort,
     })
     .withEnvironment({
-      NODE_ENV: nodeEnv,
-      LOG_LEVEL: logLevel,
+      NODE_ENV: 'production',
+      LOG_LEVEL: 'trace',
       DB_HOST: dbHost,
-      DB_NAME: postgresDb,
-      DB_USERNAME: postgresUser,
-      DB_PASSWORD: postgresPassword,
+      DB_NAME: 'veritable-ui',
+      DB_USERNAME: 'postgres',
+      DB_PASSWORD: 'postgres',
       DB_PORT: postgresPort,
-      COOKIE_SESSION_KEYS: cookieSessionKeys,
+      COOKIE_SESSION_KEYS: 'secret',
       PUBLIC_URL: publicUrl,
-      IDP_CLIENT_ID: idpClientId,
-      IDP_PUBLIC_URL_PREFIX: idpPublicUrlPrefix,
-      IDP_INTERNAL_URL_PREFIX: idpInternalUrlPrefix,
+      IDP_CLIENT_ID: 'veritable-ui',
+      IDP_PUBLIC_URL_PREFIX: 'http://localhost:3080/realms/veritable/protocol/openid-connect',
+      IDP_INTERNAL_URL_PREFIX: 'http://keycloak:8080/realms/veritable/protocol/openid-connect',
       CLOUDAGENT_ADMIN_ORIGIN: cloudagentAdminOrigin,
       CLOUDAGENT_ADMIN_WS_ORIGIN: cloudagentAdminWsOrigin,
-      INVITATION_PIN_SECRET: invitationPinSecret,
+      INVITATION_PIN_SECRET: 'secret',
       INVITATION_FROM_COMPANY_NUMBER: invitationFromCompanyNumber,
-      ISSUANCE_DID_POLICY: issuanceDidPolicy,
-      ISSUANCE_SCHEMA_POLICY: issuanceSchemaPolicy,
-      ISSUANCE_CRED_DEF_POLICY: issuanceCredDefPolicy,
-      SMTP_HOST: smtpHost,
-      SMTP_PASS: smtpPass,
-      SMTP_PORT: smtpPort,
-      SMTP_USER: smtpUser,
-      EMAIL_TRANSPORT: emailTransport,
-      COMPANY_HOUSE_API_URL: companyHouseApiUrl,
-      DEMO_MODE: demoMode,
-      SMTP_SECURE: smtpSecure,
+      ISSUANCE_DID_POLICY: 'EXISTING_OR_NEW',
+      ISSUANCE_SCHEMA_POLICY: 'EXISTING_OR_NEW',
+      ISSUANCE_CRED_DEF_POLICY: 'EXISTING_OR_NEW',
+      SMTP_HOST: 'smtp4dev',
+      SMTP_PASS: '',
+      SMTP_PORT: '25',
+      SMTP_USER: '',
+      EMAIL_TRANSPORT: 'SMTP_EMAIL',
+      COMPANY_HOUSE_API_URL: 'https://api.company-information.service.gov.uk',
+      DEMO_MODE: 'true',
+      SMTP_SECURE: 'false',
       COMPANY_PROFILE_API_KEY: companyProfileApiKey,
     })
     .withCommand([
