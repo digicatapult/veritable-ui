f0fbe92a7bd2cd91088bf2599b3009b569f989ab
Robert M. Learney <39194639+rmlearney-digicatapult@users.noreply.github.com>
2025-07-01 13:28:38 +0100

    fix/VR-364 event emitter log (#424)
    * Fix event emitter log message

--- src/utils/indexedAsyncEventEmitter.ts
+++ src/utils/indexedAsyncEventEmitter.ts
@@ -51,7 +51,7 @@ export default abstract class IndexedAsyncEventEmitter<
   // handle if async handlers throw. Non indexed events will follow what's in `options.retryExhaustedBehaviour`
   [captureRejectionSymbol]<E>(err: Error, eventName: E | K, ...args: object[]) {
     this.logger.warn('Error processing %s event: %s', eventName, err.message)
-    this.logger.debug('Error processing %s event: %o stack: %s', args[0], err.stack)
+    this.logger.debug('Error processing %s event: %o stack: %s', eventName, args[0], err.stack)
 
     const data = args[0]
     if (this.isIndexedEvent(data) && this.isTrackedEventName(eventName)) {
@@ -66,7 +66,7 @@ export default abstract class IndexedAsyncEventEmitter<
     }
 
     if (this.options.retryExhaustedBehaviour === 'THROW') {
-      this.logger.fatal('Error processing %s event: %o stack: %s', args[0], err.stack)
+      this.logger.fatal('Error processing %s event: %o stack: %s', eventName, args[0], err.stack)
       throw err
     }
   }
