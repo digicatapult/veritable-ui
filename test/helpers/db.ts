5496908096bc5e69e6d80433dee51d085a93c191
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-07-03 13:26:50 +0100

    Move migrate database into init

--- test/helpers/db.ts
+++ test/helpers/db.ts
@@ -1,21 +1,10 @@
 import { container } from 'tsyringe'
 
-import knex from 'knex'
 import Database from '../../src/models/db/index.js'
 import { tablesList } from '../../src/models/db/types.js'
-import { aliceDbConfig } from './fixtures.js'
 
 const db = container.resolve(Database)
-const database = knex(aliceDbConfig)
 
 export async function cleanupDatabase() {
   tablesList.forEach(async (table) => await db.delete(table, {}))
-}
-
-export async function migrateDatabase() {
-  try {
-    await database.migrate.latest()
-  } catch (error) {
-    throw new Error(`Unknown error in db migratiion ${error}`)
-  }
 }
