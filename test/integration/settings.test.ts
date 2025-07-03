324a7703c546dc5f1f455631e2b3231e5dd9d7c3
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-07-03 10:32:36 +0100

    use in settings

--- test/integration/settings.test.ts
+++ test/integration/settings.test.ts
@@ -1,26 +1,15 @@
 import { expect } from 'chai'
-import express from 'express'
 import { container } from 'tsyringe'
 import Database from '../../src/models/db/index.js'
-import VeritableCloudagent from '../../src/models/veritableCloudagent/index.js'
 import createHttpServer from '../../src/server.js'
-import VeritableCloudagentEvents from '../../src/services/veritableCloudagentEvents.js'
+import type { TwoPartyConnection } from '../helpers/connection.js'
 import { cleanupDatabase } from '../helpers/db.js'
 import { get, post } from '../helpers/routeHelper.js'
 import { withAdminEmail } from '../helpers/settings.js'
 
 describe('integration tests for settings page', function () {
-  const db = container.resolve(Database)
-  type Context = {
-    app: express.Express
-    cloudagentEvents: VeritableCloudagentEvents
-    remoteDatabase: Database
-    remoteCloudagent: VeritableCloudagent
-    remoteConnectionId: string
-    localConnectionId: string
-    response: Awaited<ReturnType<typeof post>>
-  }
-  const context: Context = {} as Context
+  const context: TwoPartyConnection = {} as TwoPartyConnection
+
   const testBody = {
     company_name: 'Test Company Name',
     companies_house_number: '0000000',
@@ -39,7 +28,8 @@ describe('integration tests for settings page', function () {
 
   beforeEach(async () => {
     await cleanupDatabase()
-    await withAdminEmail(db)
+    context.localDatabase = container.resolve(Database)
+    await withAdminEmail(context.localDatabase)
     const server = await createHttpServer()
     Object.assign(context, {
       ...server,
@@ -58,11 +48,11 @@ describe('integration tests for settings page', function () {
       expect(response.text.length).to.be.greaterThan(0)
     })
     it('returns success in edit mode and updates the db ', async function () {
-      const setting = await db.get('settings')
+      const setting = await context.localDatabase.get('settings')
       expect(setting.length).to.equal(1)
       expect(setting[0].setting_value).to.contain('admin@testmail.com')
       const response = await post(context.app, '/settings/?edit=true', testEditBody)
-      const setting1 = await db.get('settings')
+      const setting1 = await context.localDatabase.get('settings')
       expect(setting1.length).to.equal(1)
       expect(setting1[0].setting_value).to.contain('madmin@testmail.com')
       expect(response.status).to.equal(200)
