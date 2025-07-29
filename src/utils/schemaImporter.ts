5334d3ba9223ef7445acbda72910ab2f1fa0d204
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-07-29 09:07:13 +0100

    fixed lint

--- src/utils/schemaImporter.ts
+++ src/utils/schemaImporter.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises'
 import { SchemaDefinition } from '../models/credentialSchema'
 
 export async function loadSchema(pathToFile: string): Promise<SchemaDefinition> {
-    const file = await fs.readFile(pathToFile, 'utf-8')
-    const parsed = JSON.parse(file) as SchemaDefinition
+  const file = await fs.readFile(pathToFile, 'utf-8')
+  const parsed = JSON.parse(file) as SchemaDefinition
   return parsed
 }
