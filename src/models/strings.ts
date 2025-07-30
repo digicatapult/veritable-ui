07173e242489a7aae978a669cd2c5e0c71cc97f9
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-07-30 19:19:13 +0100

    Add types from cloudagent

--- src/models/strings.ts
+++ src/models/strings.ts
@@ -1,6 +1,26 @@
 import en from 'i18n-iso-countries/langs/en.json' with { type: 'json' }
 
 /**
+ * @example "did:key:z6Mkk7yqnGF3YwTrLpqrW6PGsKci7dNqh1CjnvMbzrMerSeL"
+ */
+export type DID = string
+
+/**
+ * @example "WgWxqztrNooG92RXvxSTWv:3:CL:20:tag"
+ */
+export type CredentialDefinitionId = string
+
+/**
+ * @example "WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0"
+ */
+export type SchemaId = string
+
+/**
+ * @example "1.0.0"
+ */
+export type Version = string
+
+/**
  * Stringified UUIDv4.
  * @pattern [0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-4[0-9A-Fa-f]{3}-[89ABab][0-9A-Fa-f]{3}-[0-9A-Fa-f]{12}
  * @example "52907745-7672-470e-a803-a2f8feb52944"
