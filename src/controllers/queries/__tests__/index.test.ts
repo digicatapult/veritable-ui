2d20b8aec05249e4d6508bba700ecd1adac82503
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-08-01 21:44:17 +0100

    update partial-query snapshot

--- src/controllers/queries/__tests__/index.test.ts
+++ src/controllers/queries/__tests__/index.test.ts
@@ -10,7 +10,7 @@ import { mockIds, toHTMLString, withQueriesMocks } from './helpers.js'
 
 const expiresAtExpectation = new Date(1000 + 7 * 24 * 60 * 60 * 1000)
 
-describe.only('QueriesController', () => {
+describe('QueriesController', () => {
   const req = { log: mockLogger } as unknown as Request
   let clock: sinon.SinonFakeTimers | null = null
 
