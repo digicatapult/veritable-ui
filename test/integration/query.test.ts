e4e72a4b18e0b8e362a88da9b5cb36e0d568dddc
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-08-15 22:22:21 +0100

    first refactor of query test

--- test/integration/query.test.ts
+++ test/integration/query.test.ts
@@ -7,38 +7,38 @@ import { insertCompanyHouseRegistry } from '../helpers/registries.js'
 import { post } from '../helpers/routeHelper.js'
 import { delay } from '../helpers/util.js'
 
-describe('query submission', function () {
+describe.only('query submission', function () {
   const context: TwoPartyContext = {} as TwoPartyContext
 
   beforeEach(async function () {
     await setupTwoPartyContext(context)
-    await cleanupCloudagent([context.localCloudagent, context.remoteCloudagent])
-    await cleanupDatabase([context.localDatabase, context.remoteDatabase])
     await insertCompanyHouseRegistry()
   })
 
   afterEach(async () => {
     context.cloudagentEvents.stop()
+    await cleanupCloudagent([context.localCloudagent, context.remoteCloudagent])
+    await cleanupDatabase([context.localDatabase, context.remoteDatabase])
   })
 
   describe('Carbon embodiment query success', function () {
-    let response: Awaited<ReturnType<typeof post>>
-
     withVerifiedConnection(context)
 
-    beforeEach(async function () {
-      response = await post(context.app, `/queries/carbon-embodiment`, {
+    it.only('should respond 200 to a post query', async function () {
+      const response = await post(context.app, `/queries/carbon-embodiment`, {
         connectionId: context.localConnectionId,
         productId: 'Test',
         quantity: 1,
       })
-    })
-
-    it('should response 200', async function () {
       expect(response.statusCode).to.equal(200)
     })
 
     it('should set local query state to pending_their_input', async function () {
+      await post(context.app, `/queries/carbon-embodiment`, {
+        connectionId: context.localConnectionId,
+        productId: 'Test',
+        quantity: 1,
+      })
       let queryLocal: QueryRow | null | undefined
       const pollLimit = 100
       for (let i = 1; i <= pollLimit; i++) {
@@ -68,6 +68,11 @@ describe('query submission', function () {
     })
 
     it('should set local query state to pending_your_input', async function () {
+      await post(context.app, `/queries/carbon-embodiment`, {
+        connectionId: context.localConnectionId,
+        productId: 'Test',
+        quantity: 1,
+      })
       let queryRemote: QueryRow | null | undefined
       const pollLimit = 100
       for (let i = 1; i <= pollLimit; i++) {
