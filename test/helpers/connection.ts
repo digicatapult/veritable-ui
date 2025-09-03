7abed96137bfcec6f27220113b77d5ba4cef814c
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-09-03 15:39:08 +0100

    don't hardcode connection delay

--- test/helpers/connection.ts
+++ test/helpers/connection.ts
@@ -274,7 +274,6 @@ export const withVerifiedConnection = function (context: TwoPartyContext) {
       } else {
         await delay(200)
       }
-
       if (i === loopLimit) {
         throw new Error('Timeout Error initialising connection')
       }
@@ -296,6 +295,8 @@ export async function withBobAndCharlie(context: ThreePartyContext) {
   })
   const invite = (emailSendStub.args.find(([name]) => name === 'connection_invite') || [])[1].invite
   const bobsInvite = JSON.parse(Buffer.from(invite, 'base64url').toString('utf8')).inviteUrl
+
+  emailSendStub.restore()
 
   const [{ id: aliceConnectionId }] = await context.db.alice.get('connection')
   context.aliceConnectionId = aliceConnectionId
@@ -373,7 +374,18 @@ export async function withBobAndCharlie(context: ThreePartyContext) {
     validity: 'valid',
   })
 
-  await delay(1000)
+  // Await completion of did exchange process (Charlie comes last)
+  const loopLimit = 100
+  for (let i = 1; i <= loopLimit; i++) {
+    const [connectionsCharlie] = await context.db.charlie.get('connection', { id: withBob.id })
+    if (connectionsCharlie.status === 'unverified') {
+      break
+    }
+    await delay(200)
+    if (i === loopLimit) {
+      throw new Error('Timeout Error initialising connection')
+    }
+  }
 
   await context.db.alice.update('connection', { id: context.aliceConnectionId }, { status: 'verified_both' })
   await context.db.bob.update('connection', { id: withAlice.id }, { status: 'verified_both' })
@@ -382,6 +394,4 @@ export async function withBobAndCharlie(context: ThreePartyContext) {
 
   context.bobsConnections = { withAlice, withCharlie }
   context.charliesConnections = { withBob }
-
-  emailSendStub.restore()
 }
