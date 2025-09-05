f6fd18e57230e2c4ba64638f3b5e5d69e17d18c1
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-09-05 14:45:13 +0100

    small delay in cleanup

--- test/helpers/cleanup.ts
+++ test/helpers/cleanup.ts
@@ -2,6 +2,7 @@ import VeritableCloudagent from '../../src/models/veritableCloudagent/index.js'
 
 import Database from '../../src/models/db/index.js'
 import { tablesList } from '../../src/models/db/types.js'
+import { delay } from './util.js'
 
 // Cleanup one Cloudagent and its DB en-bloc
 export async function testCleanup(agent: VeritableCloudagent, db: Database) {
@@ -23,4 +24,7 @@ export async function testCleanup(agent: VeritableCloudagent, db: Database) {
   for (const credential of credentials) {
     await agent.deleteCredential(credential.id)
   }
+
+  // Small delay to allow for hangup processing by peers
+  await delay(20)
 }
