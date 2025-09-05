d9df10b44915d1d0957b9ecdcbf83558593a7d1e
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-09-05 11:58:29 +0100

    bundle cleanup of single agent

--- test/helpers/cleanup.ts
+++ test/helpers/cleanup.ts
@@ -3,35 +3,24 @@ import VeritableCloudagent from '../../src/models/veritableCloudagent/index.js'
 import Database from '../../src/models/db/index.js'
 import { tablesList } from '../../src/models/db/types.js'
 
-// Call cleanup on all UI databases in parallel but delete tables in series
-// NB more complete cleanup than e2e reset - wipes all tables
-export async function cleanupDatabase(databases: Database[]) {
-  await Promise.all(
-    databases.map(async (db) => {
-      for (const table of tablesList) {
-        await db.delete(table, {})
-      }
-    })
-  )
-}
+// Cleanup one Cloudagent and its DB en-bloc
+export async function testCleanup(agent: VeritableCloudagent, db: Database) {
+  for (const table of tablesList) {
+    await db.delete(table, {})
+  }
 
-// Cleanup all agents in series
-// Delete all connections and OOB invitations in series
-export async function cleanupCloudagent(cloudagents: VeritableCloudagent[]) {
-  for (const agent of cloudagents) {
-    const connections = await agent.getConnections()
-    for (const connection of connections) {
-      await agent.closeConnection(connection.id, true)
-    }
+  const connections = await agent.getConnections()
+  for (const connection of connections) {
+    await agent.closeConnection(connection.id, true)
+  }
 
-    const invites = await agent.getOutOfBandInvites()
-    for (const invite of invites) {
-      await agent.deleteOutOfBandInvite(invite.id)
-    }
+  const invites = await agent.getOutOfBandInvites()
+  for (const invite of invites) {
+    await agent.deleteOutOfBandInvite(invite.id)
+  }
 
-    const credentials = await agent.getCredentials()
-    for (const credential of credentials) {
-      await agent.deleteCredential(credential.id)
-    }
+  const credentials = await agent.getCredentials()
+  for (const credential of credentials) {
+    await agent.deleteCredential(credential.id)
   }
 }
