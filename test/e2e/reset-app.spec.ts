6bafd008d70aa60bf5c06bfb59904f52cb26b6eb
Robert M. Learney <39194639+rmlearney-digicatapult@users.noreply.github.com>
2025-09-11 16:40:36 +0100

    VR-483 partial query e2e test (#507)
    * setupConnection
    
    * shorter keypresses
    
    * update disconnect e2e test

--- test/e2e/reset-app.spec.ts
+++ test/e2e/reset-app.spec.ts
@@ -1,37 +1,34 @@
 import { expect, Page, test } from '@playwright/test'
 import 'reflect-metadata'
 import { cleanup, CustomBrowserContext, withLoggedInUser, withRegisteredAccount } from '../helpers/registerLogIn.js'
-import { withConnection } from '../helpers/setupConnection.js'
+import { aliceE2E, bobE2E, withConnection } from '../helpers/setupConnection.js'
 
 test.describe('Resetting app', () => {
   let context: CustomBrowserContext
   let page: Page
 
-  const AliceHost = process.env.VERITABLE_ALICE_PUBLIC_URL || 'http://localhost:3000'
-  const BobHost = process.env.VERITABLE_BOB_PUBLIC_URL || 'http://localhost:3001'
-
   test.beforeEach(async ({ browser }) => {
     test.setTimeout(15000) // withConnection() can take 7sec to complete
     context = await browser.newContext()
     page = await context.newPage()
-    await withRegisteredAccount(page, context, AliceHost)
-    await withLoggedInUser(page, context, AliceHost)
-    await withConnection(AliceHost, BobHost)
+    await withRegisteredAccount(page, context, aliceE2E.url)
+    await withLoggedInUser(page, context, aliceE2E.url)
+    await withConnection(aliceE2E, bobE2E)
   })
 
   test.afterEach(async () => {
-    await cleanup([AliceHost, BobHost])
+    await cleanup([aliceE2E.url, bobE2E.url])
     await page.close()
     await context.close()
   })
 
   test('Reset all on Alice', async () => {
     await test.step('Check there is a connection', async () => {
-      await page.goto(`${AliceHost}`, { waitUntil: 'networkidle' })
+      await page.goto(`${aliceE2E.url}`, { waitUntil: 'networkidle' })
 
       const selector = page.getByRole('link', { name: 'connections', exact: true })
       await expect(selector).toBeVisible()
-      await selector.click({ delay: 100 })
+      await selector.click({ delay: 50 })
       await page.waitForLoadState('networkidle')
 
       const statusText = page.locator('div.list-item-status[data-status="success"]')
@@ -42,19 +39,19 @@ test.describe('Resetting app', () => {
     await test.step('Click reset on Alice', async () => {
       const selector = page.getByRole('link', { name: 'settings', exact: true })
       await expect(selector).toBeVisible()
-      await selector.click({ delay: 100 })
+      await selector.click({ delay: 50 })
       await page.waitForLoadState('networkidle')
 
       await page.waitForSelector('#reset-btn')
       const [response] = await Promise.all([
         page.waitForResponse((response) => response.url().includes('/reset') && response.status() === 200),
-        page.click('#reset-btn', { delay: 100 }),
+        page.click('#reset-btn', { delay: 50 }),
       ])
       expect(response.ok()).toBe(true)
     })
 
     await test.step('Check there are no connections on Alice', async () => {
-      await page.goto(`${AliceHost}/connection`, { waitUntil: 'networkidle' })
+      await page.goto(`${aliceE2E.url}/connection`, { waitUntil: 'networkidle' })
       await page.waitForSelector('text=No Connections for that search query. Try again or add a new connection')
     })
   })
--- test/helpers/setupConnection.ts
+++ test/helpers/setupConnection.ts
@@ -2,99 +2,144 @@ import 'reflect-metadata'
 
 import { RegistryType } from '../../src/models/db/types.js'
 import { CountryCode } from '../../src/models/stringTypes.js'
+import { alice, bob, nyStateCompany, openCorporatesCompany } from './fixtures.js'
 import { fetchGet, fetchPost } from './routeHelper.js'
 import { checkEmails, clearSmtp4devMessages, extractInvite, extractPin } from './smtpEmails.js'
 import { delay } from './util.js'
 
-export async function withConnection(inviterUrl: string, receiverUrl: string) {
+export interface E2ETestCompany {
+  companyName: string
+  companyNumber: string
+  address: string
+  registryCountryCode: CountryCode
+  selectedRegistry: RegistryType
+  url: string
+}
+
+export const aliceE2E: E2ETestCompany = {
+  companyName: alice.company_name,
+  companyNumber: alice.company_number,
+  address: alice.registered_office_address.address_line_1,
+  registryCountryCode: 'GB',
+  selectedRegistry: 'company_house',
+  url: process.env.VERITABLE_ALICE_PUBLIC_URL || 'http://localhost:3000',
+}
+
+export const bobE2E: E2ETestCompany = {
+  companyName: bob.company_name,
+  companyNumber: bob.company_number,
+  address: bob.registered_office_address.address_line_1,
+  registryCountryCode: 'GB',
+  selectedRegistry: 'company_house',
+  url: process.env.VERITABLE_BOB_PUBLIC_URL || 'http://localhost:3001',
+}
+
+export const charlieE2E: E2ETestCompany = {
+  companyName: nyStateCompany.current_entity_name,
+  companyNumber: nyStateCompany.dos_id,
+  address: nyStateCompany.dos_process_address_1,
+  registryCountryCode: 'US',
+  selectedRegistry: 'ny_state',
+  url: process.env.VERITABLE_CHARLIE_PUBLIC_URL || 'http://localhost:3002',
+}
+
+export const daveE2E: E2ETestCompany = {
+  companyName: openCorporatesCompany.name,
+  companyNumber: openCorporatesCompany.company_number,
+  address: openCorporatesCompany.registered_address_in_full,
+  registryCountryCode: 'GB',
+  selectedRegistry: 'open_corporates',
+  url: process.env.VERITABLE_DAVE_PUBLIC_URL || 'http://localhost:3003',
+}
+
+export async function withConnection(inviter: E2ETestCompany, receiver: E2ETestCompany) {
   const uuidRegex = /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}/g
-  let SEARCH_TERM: string
+
+  // Ensure a clean state for SMTP4Dev before starting the test.
+  // This prevents leftover emails from previous tests from interfering with email checks.
+  await clearSmtp4devMessages()
+  await delay(100)
 
   // Create the invitation
-  await fetchPost(`${inviterUrl}/connection/new/create-invitation`, {
-    companyNumber: '04659351',
-    email: 'alice@testmail.com',
+  await fetchPost(`${inviter.url}/connection/new/create-invitation`, {
+    companyNumber: receiver.companyNumber,
+    email: 'receiver@testmail.com',
     action: 'submit',
-    registryCountryCode: 'GB' as CountryCode,
-    selectedRegistry: 'company_house' as RegistryType,
+    registryCountryCode: receiver.registryCountryCode,
+    selectedRegistry: receiver.selectedRegistry,
   })
+
+  await delay(100)
 
   // Get pin and invite for the counterparty
   const adminEmail = await checkEmails('admin@veritable.com')
   const inviterPin = await extractPin(adminEmail.id)
-  if (!inviterPin) throw new Error(`PIN for ${receiverUrl} was not found.`)
+  if (!inviterPin) throw new Error(`PIN for ${receiver.url} was not found.`)
 
-  const inviteEmail = await checkEmails('alice@testmail.com')
+  const inviteEmail = await checkEmails('receiver@testmail.com')
   const inviteBase64 = await extractInvite(inviteEmail.id)
-  if (!inviteBase64) throw new Error(`Invitation for ${receiverUrl} was not found`)
+  if (!inviteBase64) throw new Error(`Invitation for ${receiver.url} was not found`)
 
   await clearSmtp4devMessages()
 
   // Counterparty receives the invitation
-  await fetchPost(`${receiverUrl}/connection/new/receive-invitation`, {
+  await fetchPost(`${receiver.url}/connection/new/receive-invitation`, {
     invite: inviteBase64,
     action: 'createConnection',
   })
 
   // Confirm the local connection record has been created
-  SEARCH_TERM = 'DIGI'
   let inviterConnectionIdAtReceiver: string | undefined
 
   for (let attempt = 1; attempt <= 100; attempt++) {
     await delay(100)
-    const res = await fetchGet(`${receiverUrl}/connection?search=${SEARCH_TERM}`)
+    const res = await fetchGet(`${receiver.url}/connection?search=${inviter.companyName}`)
     const text = await res.text()
     inviterConnectionIdAtReceiver = text.match(uuidRegex)?.[0]
-
     if (inviterConnectionIdAtReceiver) {
       break
     }
-
     if (attempt === 100) {
-      throw new Error(`No connection ID after 100 retries at ${receiverUrl}`)
+      throw new Error(`No connection ID after 100 retries at ${receiver.url}`)
     }
   }
 
   // Submit the pin code at the receiver
-  await fetchPost(`${receiverUrl}/connection/${inviterConnectionIdAtReceiver}/pin-submission`, {
+  await fetchPost(`${receiver.url}/connection/${inviterConnectionIdAtReceiver}/pin-submission`, {
     action: 'submitPinCode',
     pin: inviterPin,
-    stepCount: '3',
+    stepCount: '2',
   })
 
   // Get the responding pin code from the counterparty to the original inviter
   const receiverEmail = await checkEmails('admin@veritable.com')
   const receiverPin = await extractPin(receiverEmail.id)
-  if (!receiverPin) throw new Error(`PIN for ${inviterUrl} was not found.`)
+  if (!receiverPin) throw new Error(`PIN for ${inviter.url} was not found.`)
 
   await clearSmtp4devMessages()
 
   // Get the connection id for the counterparty at the original inviter
-  const connections = await fetchGet(`${inviterUrl}/connection?search=OFFSHORE`)
+  const connections = await fetchGet(`${inviter.url}/connection?search=${encodeURIComponent(receiver.companyName)}`)
   const [receiverConnectionIdAtInviter] = (await connections.text()).match(uuidRegex) || []
-  if (!receiverConnectionIdAtInviter) throw new Error(`No connection ID at ${inviterUrl}`)
+  if (!receiverConnectionIdAtInviter) throw new Error(`No connection ID at ${inviter.url}`)
 
   // Original inviter submits the pin code for the counterparty
-  await fetchPost(`${inviterUrl}/connection/${receiverConnectionIdAtInviter}/pin-submission`, {
+  await fetchPost(`${inviter.url}/connection/${receiverConnectionIdAtInviter}/pin-submission`, {
     action: 'submitPinCode',
     pin: receiverPin,
     stepCount: '2',
   })
 
   // Confirm the connection is successful at the original inviter
-  SEARCH_TERM = 'OFFSHORE'
-
   for (let attempt = 1; attempt <= 100; attempt++) {
     await delay(100)
-    const res = await fetchGet(`${inviterUrl}/connection?search=${SEARCH_TERM}`)
+    const res = await fetchGet(`${inviter.url}/connection?search=${encodeURIComponent(receiver.companyName)}`)
     const text = await res.text()
-
     if (/data-status="success"/.test(text)) {
       return
     }
-
     if (attempt === 100) {
-      throw new Error(`Connection unsuccessful after 100 retries: ${inviterUrl}`)
+      throw new Error(`Connection unsuccessful after 100 retries: ${inviter.url}`)
     }
   }
 }
