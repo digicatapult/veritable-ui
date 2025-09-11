ac42765e348597cf4447f0e78fa7829b5ff98c36
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-09-11 14:01:22 +0100

    update disconnect e2e test

--- test/e2e/profile-and-disconnect-connection.spec.ts
+++ test/e2e/profile-and-disconnect-connection.spec.ts
@@ -1,11 +1,8 @@
 import { expect, Page, test } from '@playwright/test'
 import { cleanup, CustomBrowserContext, withLoggedInUser, withRegisteredAccount } from '../helpers/registerLogIn.js'
-import { withConnection } from '../helpers/setupConnection.js'
+import { aliceE2E, bobE2E, withConnection } from '../helpers/setupConnection.js'
 
 test.describe('Disconnect connection', () => {
-  const AliceHost = process.env.VERITABLE_ALICE_PUBLIC_URL || 'http://localhost:3000'
-  const BobHost = process.env.VERITABLE_BOB_PUBLIC_URL || 'http://localhost:3001'
-
   let context: CustomBrowserContext
   let page: Page
 
@@ -13,20 +10,20 @@ test.describe('Disconnect connection', () => {
     test.setTimeout(15000) // withConnection() can take 7sec to complete
     context = await browser.newContext()
     page = await context.newPage()
-    await withRegisteredAccount(page, context, AliceHost)
-    await withLoggedInUser(page, context, AliceHost)
-    await withConnection(AliceHost, BobHost)
+    await withRegisteredAccount(page, context, aliceE2E.url)
+    await withLoggedInUser(page, context, aliceE2E.url)
+    await withConnection(aliceE2E, bobE2E)
   })
 
   test.afterEach(async () => {
-    await cleanup([AliceHost, BobHost])
+    await cleanup([aliceE2E.url, bobE2E.url])
     await page.close()
     await context.close()
   })
 
   test('views a connection page (Bob)', async () => {
     await test.step('go to the profile page of an established connection', async () => {
-      await page.goto(`${AliceHost}/connection`, { waitUntil: 'networkidle' })
+      await page.goto(`${aliceE2E.url}/connection`, { waitUntil: 'networkidle' })
 
       await page.click('a[href*="/connection/profile/"]:has-text("View Connection")')
 
@@ -34,9 +31,9 @@ test.describe('Disconnect connection', () => {
       await page.waitForLoadState('networkidle')
     })
     await test.step('check profile page shows all expected info', async () => {
-      await expect(page.getByRole('heading', { name: 'OFFSHORE RENEWABLE ENERGY' })).toBeVisible()
-      await expect(page.getByText('GB')).toBeVisible()
-      await expect(page.getByText('04659351')).toBeVisible()
+      await expect(page.getByRole('heading', { name: bobE2E.companyName })).toBeVisible()
+      await expect(page.getByText(String(bobE2E.registryCountryCode))).toBeVisible()
+      await expect(page.getByText(bobE2E.companyNumber)).toBeVisible()
 
       // Check connection status is visible and shows "Verified"
       await expect(page.locator('b.connection-status[data-status="success"]')).toBeVisible()
@@ -46,21 +43,23 @@ test.describe('Disconnect connection', () => {
       await expect(page.locator('#disconnect-button')).toBeVisible()
       await expect(page.getByText('Disconnect')).toBeVisible()
       await expect(page.locator('#disconnect-button-warning')).toBeVisible()
-      await expect(page.getByText('This action will revoke connection with OFFSHORE RENEWABLE')).toBeVisible()
+      await expect(page.getByText(`This action will revoke connection with ${bobE2E.companyName}`)).toBeVisible()
     })
 
     await test.step('click disconnect button', async () => {
       await page.click('#disconnect-button')
       await page.waitForLoadState('networkidle')
-      await expect(page.getByText('Are you sure you want to revoke connection with OFFSHORE RENEWABLE')).toBeVisible()
+      await expect(
+        page.getByText(`Are you sure you want to revoke connection with ${bobE2E.companyName}`)
+      ).toBeVisible()
       await page.click('#final-disconnect-button')
       await page.waitForLoadState('networkidle')
     })
 
     await test.step('check profile page shows all expected info', async () => {
-      await expect(page.getByRole('heading', { name: 'OFFSHORE RENEWABLE ENERGY' })).toBeVisible()
-      await expect(page.getByText('GB')).toBeVisible()
-      await expect(page.getByText('04659351')).toBeVisible()
+      await expect(page.getByRole('heading', { name: bobE2E.companyName })).toBeVisible()
+      await expect(page.getByText(String(bobE2E.registryCountryCode))).toBeVisible()
+      await expect(page.getByText(bobE2E.companyNumber)).toBeVisible()
 
       // Check connection status is visible and shows "Verified"
       await expect(page.getByText('This connection has been')).toBeVisible()
